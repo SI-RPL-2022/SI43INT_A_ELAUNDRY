cc6bab3acdafe9557322fed1478f6d847a08b5e5
BAGAS <bagaszakwa@student.telkomuniversity.ac.id>
2022-06-18 04:16:12 +0700

    fix new

--- E_Laundry/resources/views/Service/order.blade.php
+++ E_Laundry/resources/views/Service/order.blade.php
@@ -77,6 +77,8 @@ a:focus,a:active {
     border:1px solid #4bb8a9;
 }
 
+
+
 .card-2{
     margin-top: 40px !important;
 }
@@ -101,11 +103,6 @@ a:focus,a:active {
     cursor: pointer;
 }
 
-/* .boxed {
-    padding: 0px 8px 0 8px ;
-    background-color: #4bb8a9;
-    color: white;
-} */
 
 .boxed-1{
     padding: 0px 8px 0 8px ;
@@ -120,17 +117,17 @@ a:focus,a:active {
 }
 </style>
 
-<div class="col-md-5">
+<div class="col-md-4 mx-auto">
     <div class="card border-0 ">
         <div class="card-header card-2">
-            <p class="card-text text-muted mt-md-4  mb-2 space">YOUR ORDER</p>
+            <h3 class="card-text text-muted mt-md-4  mb-2 space">YOUR ORDER</h3>
             <hr class="my-2">
         </div>
         <div class="card-body pt-0">
             <div class="row  justify-content-between">
                 <div class="col-auto col-md-7">
                     <div class="media flex-column flex-sm-row">
-                        <img class=" img-fluid" src="https://images.unsplash.com/photo-1635274605638-d44babc08a4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80\" width="62" height="62">
+                        <img class=" img-fluid" src="https://images.unsplash.com/photo-1635274605638-d44babc08a4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80\" width="120" height="120">
                         <div class="media-body  my-auto">
                             <div class="row ">
                                 <div class="col-auto"><p class="mb-0"><b>Dry Laundry</b></div>
@@ -160,8 +157,8 @@ a:focus,a:active {
                 </div>
             </div>
             <div class="row mb-5 mt-4 ">
-                <div class="col-md-7 col-lg-6 mx-auto"><button type="button" class="btn btn-block btn-outline-primary btn-lg">ADD GIFT CODE</button></div>
-            <div class="col-3"><a class="btn btn-warning" href="{{ url ('/invoice') }}">Complete Order</a></div>
+                <div class="col-lg-6"><button type="button" class="btn btn-outline-primary btn-lg my-2">ADD GIFT CODE</button></div>
+                <div class="col-lg-4"><a class="btn btn-warning my-2" href="{{ url ('/invoice') }}">Complete Order</a></div>
             </div>
             
         </div>
