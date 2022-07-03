c32d5e5919c4a73683610a0598e45b3e518467bf
gitnyaDanil <danielpolii19@gmail.com>
2022-07-03 18:54:08 +0700

    Create Table Form, Shipping, Payment

---
+++ E_Laundry/database/migrations/2022_06_27_231646_create_shippings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shippings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('delivery');
+            $table->string('address');
+            $table->string('street');
+            $table->string('post_code');        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shippings');
+    }
+}
